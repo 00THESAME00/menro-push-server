b433a18e0640840628edfcb9362fbfa6303d5603
Emil Onuchin <onuchinemil@gmail.com>
2025-07-17 22:01:04 +0500

    Обновление

--- lib/main.dart
+++ lib/main.dart
@@ -638,7 +638,7 @@ class _ChatListScreenState extends State<ChatListScreen> with TickerProviderStat
               top: true,
               child: Container(
                 color: Colors.black,
-                padding: const EdgeInsets.fromLTRB(20, 56, 20, 20), // ← поднимал чаты выше
+                padding: const EdgeInsets.fromLTRB(12, 12, 12, 6),
                 child: Column(
                   children: [
                     Row(
@@ -727,7 +727,7 @@ class _ChatListScreenState extends State<ChatListScreen> with TickerProviderStat
                 }
 
                 return ListView.separated(
-                  padding: const EdgeInsets.fromLTRB(20, 32, 20, 20),
+                  padding: const EdgeInsets.fromLTRB(20, 56, 20, 20), // ← поднимал чаты выше
                   itemCount: chats.length,
                   separatorBuilder: (_, __) => const SizedBox(height: 14),
                   itemBuilder: (_, index) {
